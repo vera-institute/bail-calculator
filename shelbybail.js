5a1280e20044a92a4f1ea65bb3708bb4b442bcb2
Sara Lanclos <slanclos@vera.org>
2022-07-27 17:52:20 -0400

    Add HTML/CSS/JS files

---
+++ shelbybail.js
@@ -0,0 +1,149 @@
+// incomeHourlyOrSalaryAndRadioButtonsAndInputs
+$(document).ready(function(){
+    $(".radioSelect").each(function(){
+        showSpecificFields(this);
+    });
+    $(".radioSelect").click(function(){
+    showSpecificFields(this);
+    });
+});
+function showSpecificFields(obj){
+    if($(obj).is(":checked")){
+    var radioVal = $(obj).val();
+     $(".fieldsSpecific").each(function(){
+         if($(this).attr('id') == radioVal){
+             $(this).show();
+         } else{
+             $(this).hide();                 
+         }
+     });   
+    }
+}
+
+
+
+// income
+function hourlySalaryCheck() {
+    hourly1 = document.getElementById('hourlyWage')
+    hourly2 = document.getElementById('hoursPerWeek')
+    
+    salary1 = document.getElementById('annualSalary')
+    salary2 = document.getElementById('monthlySalary')
+    
+    if (document.getElementById('hourlyCheck').checked) {
+        hourly1.type = hourly2.type = 'number';
+        salary1.type = salary2.type = 'hidden';
+    } else {
+        salary1.type = salary2.type = 'number';
+        hourly1.type = hourly2.type = 'hidden';
+    }
+}
+
+var totalIncome
+
+function incomeCompute()
+{
+    var hourlyWageValue = parseInt(document.getElementById("hourlyWage").value);
+    var hoursPerWeekValue = parseInt(document.getElementById("hoursPerWeek").value);
+    var annualSalaryValue = parseInt(document.getElementById("annualSalary").value);
+    var monthlySalaryValue = parseInt(document.getElementById("monthlySalary").value);
+    
+    // var totalIncome = ((hourlyWageValue * hoursPerWeekValue)*4);
+ 
+    if (annualSalaryValue > 0) {        
+        totalIncome = annualSalaryValue / 12
+    } else if (hourlyWageValue > 0){
+       totalIncome = hourlyWageValue * hoursPerWeekValue
+    }else{
+       totalIncome = "Please Enter Values";
+    }
+    document.getElementById("totalIncome").innerHTML = totalIncome
+
+document.getElementById("totes-income").innerText = totalIncome
+
+}
+
+
+
+// incomeFromBenefits
+function incomeFromBenefitsCompute()
+{
+    var cashBenefitsValue = parseInt(document.getElementById("cashBenefits").value);
+    var unemploymentValue = parseInt(document.getElementById("unemployment").value);
+    var SSIValue = parseInt(document.getElementById("SSI").value);
+    var SSDValue = parseInt(document.getElementById("SSD").value);
+    var socialSecurityValue = parseInt(document.getElementById("socialSecurity").value);
+    var pensionRetirementValue = parseInt(document.getElementById("pensionRetirement").value);
+    var anyOtherIncomeValue = parseInt(document.getElementById("anyOtherIncome").value);
+ 
+    var totalIncomeFromBenefits = cashBenefitsValue + unemploymentValue + SSIValue + SSDValue + socialSecurityValue + pensionRetirementValue + anyOtherIncomeValue;
+
+    if (totalIncomeFromBenefits)
+    {
+        document.getElementById("totalIncomeFromBenefits").innerHTML = totalIncomeFromBenefits;
+    }
+    else
+    {
+        document.getElementById("totalIncomeFromBenefits").innerHTML = "Please Enter Values";
+    }
+}
+// liquidAssets
+function liquidAssetsCompute()
+{
+    var checkingAccountValue = parseInt(document.getElementById("checkingAccount").value);
+    var savingsAccountValue = parseInt(document.getElementById("savingsAccount").value);
+    var cashAvailableNowValue = parseInt(document.getElementById("cashAvailableNow").value);  
+    var otherAssetsValue = parseInt(document.getElementById("otherAssets").value);
+ 
+    var totalLiquidAssets = checkingAccountValue + savingsAccountValue + cashAvailableNowValue + otherAssetsValue;
+ 
+    if (totalLiquidAssets)
+    {
+        document.getElementById("totalLiquidAssets").innerHTML = totalLiquidAssets;
+    }
+    else
+    {
+        document.getElementById("totalLiquidAssets").innerHTML = "Please Enter Values";
+    }
+}
+
+// Expenses
+function expensesCompute()
+{
+    var housingValue = parseInt(document.getElementById("payHousing").value);
+    var electricityGasValue = parseInt(document.getElementById("payElectricityGas").value);
+    var foodGroceryValue = parseInt(document.getElementById("payFoodGrocery").value);
+    var transportationValue = parseInt(document.getElementById("payTransportation").value);
+    var phoneValue = parseInt(document.getElementById("payPhone").value);
+    var studentLoansValue = parseInt(document.getElementById("payStudentLoans").value);
+    var childSupportValue = parseInt(document.getElementById("payChildSupport").value);
+    var medicalValue = parseInt(document.getElementById("payMedical").value);
+    var tvInternetValue = parseInt(document.getElementById("payTvInternet").value);
+    var otherValue = parseInt(document.getElementById("payOther").value);
+ 
+    var totalExpenses = housingValue + electricityGasValue + foodGroceryValue + transportationValue + phoneValue + studentLoansValue + childSupportValue + medicalValue +  tvInternetValue + otherValue;
+ 
+    if (totalExpenses)
+    {
+        document.getElementById("totalExpenses").innerHTML = totalExpenses;
+    }
+    else
+    {
+        document.getElementById("totalExpenses").innerHTML = "Please Enter Values";
+    }
+}
+
+// var number = 123;
+// var number = parseInt(document.getElementById("totalExpenses").value);
+
+// let number = document.getElementById("totalExpenses").innerHTML;
+// document.write(number);
+
+// function tester() {
+//     document.getElementById("totalExpenses").innerHTML = X;
+// }
+// var example = 'Cats are awesome!';
+// document.write(example);
+
+// var totalExpenses = totalExpenses;
+// document.write(totalExpenses() );
